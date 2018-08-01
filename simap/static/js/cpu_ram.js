54871eb1fdc8baf2006f53b848bbed67feaca661
ddyh2901 <ddyh2901@naver.com>
2018-08-01 14:59:25 +0900

    commit

--- simap/static/js/cpu_ram.js
+++ simap/static/js/cpu_ram.js
@@ -1,7 +1,3 @@
-    var data = [];
-    var data2 = [];
-    var dataset, dataset2;
-    var totalPoints = 50;
     var updateInterval = 1000;
     var now = new Date().getTime();
     var count = 0;
@@ -13,17 +9,10 @@
                 url: 'http://127.0.0.1:8000/simap/info/cpu-ram',
                 dataType: 'json',
                 success: function(result){
-                  if(data.length > totalPoints){
-                    data.shift();
-                    data2.shift();
-                  }
+
                     var w = result.time;
                     var y = result.cpu;
                     var x = result.ram;
-                    var temp = [now += updateInterval, y];
-                    var temp2 = [now += updateInterval, x];
-                    data.push(temp);
-                    data2.push(temp2);
 
                     if(y > 75){
                         if(count<5)
@@ -82,6 +71,51 @@
 $(document).ready(function() {
 
         var nowTime = new Date().getTime();
+
+        $.ajax({
+                type: 'GET',
+                url: 'http://127.0.0.1:8000/Log_five',
+                dataType: 'json',
+                success: function(result){
+                    var i;
+                    for(i=4;i>=0;i--)
+                     {
+                            var w = result[i].time;
+                            var y = result[i].cpu;
+                            var x = result[i].ram;
+                            z = result[i].check;
+
+                            if(z === true){
+                                if(y > 75){
+                                    count++;
+                                var html = '<div class="dropdown-divider" id="div_' + count + '"></div>';
+                                    html += '<a class="dropdown-item" id="a_' + count + '" href="log.html">';
+                                    html += '<span class="text-danger">';
+                                    html += '<strong>';
+                                    html += '<i class="fa fa-long-arrow-up fa-fw"></i>CPU 75% 이상 사용!</strong>';
+                                    html += '</span>';
+                                    html += '<span class="small float-right text-muted">' + w + '</span>';
+                                    html += '<div class="dropdown-message small">This is an automated server response message. All systems are online.</div></a>';
+
+                                $("#starting").prepend(html);
+                            }
+
+                            if(x > 75){
+                                    count++;
+                                var html = '<div class="dropdown-divider" id="div_' + count + '"></div>';
+                                    html += '<a class="dropdown-item" id="a_' + count + '" href="#">';
+                                    html += '<span class="text-success">';
+                                    html += '<strong>';
+                                    html += '<i class="fa fa-long-arrow-up fa-fw"></i>메모리 75% 이상 사용!</strong>';
+                                    html += '</span>';
+                                    html += '<span class="small float-right text-muted">' + w + '</span>';
+                                    html += '<div class="dropdown-message small">This is an automated server response message. All systems are online.</div></a>';
+                                    $("#starting").prepend(html);
+                              }
+                            }
+                      }
+                }
+         });
 
         function update() {
             GetData1();
